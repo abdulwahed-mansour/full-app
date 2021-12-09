1c98046241146a70a13309791ee4d63e25662528
abdulwahed-mansour <abdulwahed.mansour@gmail.com>
2021-12-09 22:42:30 +0200

    Add about page test

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def homepage(request):
-  return render(request, 'index.html', context={})
+  return render(request, 'pages/index.html', context={})
+
+
+def aboutpage(request):
+  return render(request, 'pages/about.html', context={})
